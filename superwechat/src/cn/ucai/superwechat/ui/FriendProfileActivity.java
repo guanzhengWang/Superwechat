f326c1e2c24e047e663524b8402abb36406255b9
guanzhengWang <610085827@qq.com>
2017-02-14 19:41:01 +0800

    查找到的好友界面和发送添加好友请求

--- superwechat/src/cn/ucai/superwechat/ui/FriendProfileActivity.java
+++ superwechat/src/cn/ucai/superwechat/ui/FriendProfileActivity.java
@@ -1,13 +1,95 @@
 package cn.ucai.superwechat.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class FriendProfileActivity extends AppCompatActivity {
+import com.hyphenate.easeui.domain.User;
+import com.hyphenate.easeui.utils.EaseUserUtils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import cn.ucai.superwechat.I;
+import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.SuperWeChatHelper;
+import cn.ucai.superwechat.utils.MFGT;
+
+public class FriendProfileActivity extends BaseActivity {
+
+    @BindView(R.id.img_back)
+    ImageView imgBack;
+    @BindView(R.id.txt_title)
+    TextView txtTitle;
+    @BindView(R.id.user_head_avatar)
+    ImageView userHeadAvatar;
+    @BindView(R.id.tv_usernick)
+    TextView tvUsernick;
+    @BindView(R.id.tv_username)
+    TextView tvUsername;
+    @BindView(R.id.btn_sendMessage)
+    Button btnSendMessage;
+    @BindView(R.id.btn_sendVideo)
+    Button btnSendVideo;
+    @BindView(R.id.btn_addContact)
+    Button btnAddContact;
+    User user = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_friend_profile);
+        setContentView(R.layout.activity_friend_profile);
+        ButterKnife.bind(this);
+        initData();
+    }
+
+    private void initData() {
+        imgBack.setVisibility(View.VISIBLE);
+        txtTitle.setVisibility(View.VISIBLE);
+        txtTitle.setText(R.string.title_user_profile);
+
+        user = (User) getIntent().getSerializableExtra(I.User.USER_NAME);
+        if (user != null) {
+            showUserInfo();
+        } else {
+            MFGT.finish(this);
+        }
+    }
+
+    private void showUserInfo() {
+        tvUsernick.setText(user.getMUserNick());
+        EaseUserUtils.setUserAvatarbyPath(this, user.getMUserName(), userHeadAvatar);
+        tvUsername.setText("微信号:" + user.getMUserName());
+        if (isFriend()) {
+            btnSendMessage.setVisibility(View.VISIBLE);
+            btnSendVideo.setVisibility(View.VISIBLE);
+        } else {
+            btnAddContact.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private boolean isFriend() {
+        User u = SuperWeChatHelper.getInstance().getAppContactList().get(user.getMUserName());
+        if (u == null) {
+            return false;
+        } else {
+            SuperWeChatHelper.getInstance().saveAppContact(user);
+            return true;
+        }
+    }
+
+    @OnClick(R.id.img_back)
+    public void imgBack() {
+        MFGT.finish(this);
+    }
+
+    @OnClick(R.id.btn_addContact)
+    public void addContact() {
+        Log.e("FriendProfile","添加好友"+user);
+        MFGT.gotoAddFriendActivity(this,user.getMUserName());
     }
 }
--- superwechat/src/cn/ucai/superwechat/utils/MFGT.java
+++ superwechat/src/cn/ucai/superwechat/utils/MFGT.java
@@ -7,8 +7,11 @@ import android.support.v4.app.FragmentActivity;
 
 import com.hyphenate.easeui.domain.User;
 
+import cn.ucai.superwechat.I;
 import cn.ucai.superwechat.R;
 import cn.ucai.superwechat.ui.AddContactActivity;
+import cn.ucai.superwechat.ui.AddFriendActivity;
+import cn.ucai.superwechat.ui.FriendProfileActivity;
 import cn.ucai.superwechat.ui.GuideActivity;
 import cn.ucai.superwechat.ui.LoginActivity;
 import cn.ucai.superwechat.ui.RegisterActivity;
@@ -59,6 +62,10 @@ public class MFGT {
     }
 
     public static void gotoFriend(Activity activity, User user) {
+        startActivity(activity,new Intent(activity,FriendProfileActivity.class).putExtra(I.User.USER_NAME,user));
+    }
 
+    public static void gotoAddFriendActivity(Activity activity, String username) {
+        startActivity(activity,new Intent(activity,AddFriendActivity.class).putExtra(I.User.USER_NAME,username));
     }
 }
